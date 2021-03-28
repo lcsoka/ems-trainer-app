5d0ea499a537a810975f9a0e59dd8d993a1b07c3
Laszlo Csoka <laszlo@eclick.co.hu>
2021-03-28 01:13:33 +0100

    (feat) channel list

--- EMSTrainerApp/Pages/Main/ViewControllers/WorkoutViewController.swift
+++ EMSTrainerApp/Pages/Main/ViewControllers/WorkoutViewController.swift
@@ -12,16 +12,23 @@ class WorkoutViewController: UIViewController, MainStoryboardLodable {
     @IBOutlet var lblTimeLeft: UILabel!
     @IBOutlet var waveView: WaveView!
     @IBOutlet var progressView: ProgressView!
+    @IBOutlet var channelContainer: UIView!
+    @IBOutlet var channelCollection: UICollectionView!
     
     var viewModel: WorkoutViewModel!
     
     private var minWaveFreq: Float = 4
     private var maxWaveFreq: Float = 6
     
+    private var reuseIdentifier = "channelCell"
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         viewModel.client.connect()
         updateTime(time: viewModel.timeLeft)
+        channelCollection.delegate = self
+        channelCollection.dataSource = self
+        channelCollection.backgroundColor = .clear
     }
     
     override func viewDidLoad() {
@@ -51,6 +58,27 @@ class WorkoutViewController: UIViewController, MainStoryboardLodable {
     private func updateTime(time: Int) {
         let timeLeft = Converter.secondsToTimeString(time)
         lblTimeLeft.text = timeLeft
+    }
+}
+
+extension WorkoutViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,for: indexPath)
+
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: channelCollection.frame.height - 20, height: channelCollection.frame.height - 20)
     }
 }
 
